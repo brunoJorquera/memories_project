d95cad141b67e5852b7fbd7dcad2339c819c46d8
brunoJorquera <jorquera.b18@gmail.com>
2021-11-15 13:19:01 -0800

    Deployment practice

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-
+import dotenv from "dotenv";
 import postRoutes from "./routes/posts.js";
 
 const app = express();
+dotenv.config();
 
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
@@ -12,12 +13,16 @@ app.use(cors());
 
 app.use("/posts", postRoutes);
 
-const CONNECTION_URL =
-  "mongodb+srv://brunoJorquera:brunoJorquera123@cluster0.j1elg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+// const CONNECTION_URL =
+//   "mongodb+srv://brunoJorquera:brunoJorquera123@cluster0.j1elg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => app.listen(PORT, () => console.log(`Server Running on ${PORT}`)))
   .catch((error) => console.log(error.message));
 
